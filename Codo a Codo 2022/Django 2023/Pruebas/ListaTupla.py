1a3c03f0b93e939a576d0d1540f5e09bcb32ab29
EzeCancela <gecancela@hotmail.com>
2023-03-15 15:51:59 -0300

    modifiqué ListaTupla

--- Codo a Codo 2022/Django 2023/Pruebas/ListaTupla.py
+++ Codo a Codo 2022/Django 2023/Pruebas/ListaTupla.py
@@ -1,10 +1,11 @@
 #definimos una lista vacia
 lista=[]
 #disponemos un ciclo de 5 vueltas
-
-valor=input("Ingrese un valor entero, ingrese * para terminar: ")
-
+valor = ""
 while valor != "*":
+    valor=input("Ingrese un valor entero o ingrese * para terminar: ")
+    if valor == "*":
+        break
     lista.append(int(valor))
 
 #imprimimos la lista    
